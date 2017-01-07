b623e2773705f07f391fa2b5c8581e810f1369aa
gatchi <christen.got@gmail.com>
2017-01-06 22:43:26 -0800

    Created separate folder for 32-bit version
    Dev should be mostly focused on a 32-bit version.
    Theres not much gain and quite the headache at
    trying to dev for 64-bit.

--- 64/account_add.c
+++ 64/account_add.c
@@ -1,11 +1,12 @@
-/****************************************************************/
-/*	Author:	Sodaboy												*/
-/*	Date:	07/22/2008											*/
-/*	accountadd.c :  Adds an account to the Tethealla PSO		*/
-/*			server...											*/
-/*																*/
-/*	History:													*/
-/*		07/22/2008  TC  First version...						*/
+/***************************************************************
+/*	Author:	gatchi												
+/*	Date:	12/26/16										
+/*	accountadd.c :  Adds an account to the Ryuker PSO			
+/*			server...											
+/*															
+/*	History:													
+/*		07/22/2008  TC  First version...
+/*		12/26/2016		Start of Ryuker fork dev				
 /****************************************************************/
 
 #include <windows.h>
@@ -43,64 +44,64 @@ int main( int argc, char * argv[] )
 	char mySQL_Password[255] = {0};
 	char mySQL_Database[255] = {0};
 	unsigned int mySQL_Port;
-	int config_index = 0;
+	int config_index = 0;  // Place in the config file, i think
 	char config_data[255];
 	
 	unsigned char MDBuffer[17] = {0};
 
 	FILE* fp;
 
-	// The following reads a config file and sets some stuff up i g uess?
+	// The following reads a config file and sets some stuff up i guess?
 	if ( ( fp = fopen ("tethealla.ini", "r" ) ) == NULL )
 	{
 		printf ("The configuration file tethealla.ini appears to be missing.\n");
 		return 1;
 	}
 	else
-		while (fgets (&config_data[0], 255, fp) != NULL)
+	{
+		unsigned char len;
+		while (fgets (config_data, 255, fp) != NULL) // This reads a full line
 		{
-			if (config_data[0] != 0x23)
+			if (config_data[0] != '#')  // Symbol for comments, at beginning of line
 			{
-				if (config_index < 0x04)
-				{
-					ch = strlen (&config_data[0]);
-					if (config_data[ch-1] == 0x0A)
-						config_data[ch--]  = 0x00;
-					config_data[ch] = 0;
-				}
+				// Strip the line feed from the string
+				len = strlen (config_data);
+				if (config_data[len-1] == '\n')
+					config_data[--len]  = 0;
+				
 				switch (config_index)
 				{
-				case 0x00:
-					// MySQL Host
-					memcpy (&mySQL_Host[0], &config_data[0], ch+1);
-					break;
-				case 0x01:
-					// MySQL Username
-					memcpy (&mySQL_Username[0], &config_data[0], ch+1);
-					break;
-				case 0x02:
-					// MySQL Password
-					memcpy (&mySQL_Password[0], &config_data[0], ch+1);
-					break;
-				case 0x03:
-					// MySQL Database
-					memcpy (&mySQL_Database[0], &config_data[0], ch+1);
-					break;
-				case 0x04:
-					// MySQL Port
-					mySQL_Port = atoi (&config_data[0]);
-					break;
-				default:
-					break;
+					// Use memccpy instead of strcpy cause len is known,
+					// len+1 because null terminator
+					case 0:
+						memcpy (mySQL_Host, config_data, len+1);
+						break;
+					case 1:
+						memcpy (mySQL_Username, config_data, len+1);
+						break;
+					case 2:
+						memcpy (mySQL_Password, config_data, len+1);
+						break;
+					case 3:
+						memcpy (mySQL_Database, config_data, len+1);
+						break;
+					case 4:
+						mySQL_Port = strtol (config_data, NULL, 10);
+						break;
+					default:
+						break;
 				}
+				if (config_index > 4)
+					break;
 				config_index++;
 			}
 		}
 		fclose (fp);
-
+	}
+	
 	if (config_index < 5)
 	{
-		printf ("tethealla.ini seems to be corrupted.\n");
+		printf ("tethealla.ini is missing info or corrupted.\n");
 		return 1;
 	}
 	
