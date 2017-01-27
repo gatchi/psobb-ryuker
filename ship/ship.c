570af4062c3c05339bd2678c63328e758f8d96e8
gatchi <christen.got@gmail.com>
2017-01-27 00:08:09 -0800

    +cJSON, config file make mostly works
    Program can now produce json config files if they dont exist already,
    via user input.  However getting the port number input is proving to be
    a hassle, causing an application quit immediately after and not
    assigning the default value when left blank.

--- ship/ship.c
+++ ship/ship.c
@@ -20,12 +20,16 @@
 #endif
 
 #include <stdio.h>
+#include "cJSON.h"
 
-#define SERVER_VERSION "(alpha)"
+#define SERVER_VERSION		"(alpha)"
+#define CONFIG_FILE			"shipconfig.json"
+#define MAX_LENGTH			20
 
 int main( void )
 {
-	unsigned char dp[1000] = {0};
+	unsigned char dp[1000] = {0};  // Dummy pointer for key presses
+	unsigned char dc;  // Dummy char for key presses
 
 #ifdef _WIN32
 	// Starts off with giving the server installer this nice welcome message
@@ -38,44 +42,140 @@ int main( void )
 	
 	// Negotiate with winsock (windows networking) to get winsock data by providing version request
 	WSADATA winsock_data;
-	if ( !WSAStartup(MAKEWORD(1,0), &winsock_data) )
+	if ( !WSAStartup(MAKEWORD(2,2), &winsock_data) )
 	{
 		printf ("Recieved winsock data.\n");
-		printf ("Winsock version %x.%x\n", winsock_data.wVersion, winsock_data.wHighVersion);
+		printf ("Press [ENTER] to start setup...");
+		gets (dp);
 	}
 	else
+	{
 		printf ("Could not negotiate with winsock.\n");
+		printf ("Press [ENTER] to quit...");
+		gets (dp);
+		exit (1);
+	}
 	
-	printf ("Press [ENTER] to quit...");
-	gets(dp);
+	/*
+	   In the original file, this is the part where configuration files are loaded:
+	   - ship.ini
+	   - language file
+	   - ship_key.bin
+	   - weapon parameter file
+	   - barrier parameter file
+	   - technique parameter file
+	   - battle parameter files
+	   - param\ItemPT.gsl (common drop tables, created by SEGA, archive format)
+	   - "EP1 data"
+	   - "EP2 data"
+	   - param\PlyLevelTbl.bin (level up stats and such, made by SEGA)
+	      (uncompressed version found in login_server folder)
+	   - quests
+	   - shop.dat
+	   - configure shop settings for each level (shop indices)
 	
-	// In the original file, this is the part where configuration files are loaded:
-	//  ship.ini
-	//  language file
-	//  ship_key.bin
-	//  weapon parameter file
-	//  barrier parameter file
-	//  technique parameter file
-	//  battle parameter files
-	//  param\ItemPT.gsl
-	//  "EP1 data"
-	//  "EP2 data"
-	//  param\PlyLevelTbl.bin
-	//  quests
-	//  shop.dat
-	//  configure shop settings for each level (shop indices)
+	   It also generates a mtwist best seed right before loading the config file (ship.ini), not sure y.
+	   Its not actually used in the load_config function, nor in load_mask.
+	 */
 	
-	// Then it appears to get data from a packet (logon packet?) for setting up network
-	// parameters for the ship and for connecting to the logon server.  It then tries to connect.
+	// Try to load main config json.  If it doesnt exist, make it.
+	FILE * cf = fopen (CONFIG_FILE, "r+");
+	if (0)
+	{
+		// shit happens here
+		printf ("Config file found, press [ENTER] to exit.");
+		gets (dp);
+		fclose (cf);
+	}
+	else
+	{
+		printf ("The configuration file %s appears to be missing.\n", CONFIG_FILE);
+		printf ("Proceed or quit? [ENTER/q]: ");  // Capital letter means default
+		dc = 0;
+		dc = getchar();
+		if (dc != '\n')
+		{
+			exit (1);
+		}
+		
+		// Generate config json
+		
+		cJSON * cj = cJSON_CreateObject();
+		unsigned char mysqlhost[MAX_LENGTH];
+		unsigned char mysqldbn[MAX_LENGTH];
+		unsigned char mysqluname[MAX_LENGTH];
+		unsigned char mysqlpass[MAX_LENGTH] = {0};
+		unsigned int mysqlport[6];
+		
+		printf ("\nWelcome to the ship config creator.\n");
+		printf ("Please enter the following values:\n");
+		
+		// MySQL Host
+		printf ("MySQL host? [localhost]: ");
+		gets (mysqlhost);
+		if (mysqlhost[0] == '\0')
+			cJSON_AddStringToObject (cj, "mysqlhost", "localhost");
+		else
+			cJSON_AddStringToObject (cj, "mysqlhost", mysqlhost);
+		
+		// MySQL Database Name
+		printf ("MySQL database name? [pso_db]: ");
+		gets (mysqldbn);
+		if (mysqldbn[0] == '\0')
+			cJSON_AddStringToObject (cj, "mysqldbname", "pso_db");
+		else
+			cJSON_AddStringToObject (cj, "mysqldbname", mysqldbn);
+		
+		// MySQL Username
+		printf ("MySQL username? [pso]: ");
+		gets (mysqluname);
+		if (mysqluname[0] == '\0')
+			cJSON_AddStringToObject (cj, "mysqluname", "pso");
+		else
+			cJSON_AddStringToObject (cj, "mysqluname", mysqlhost);
+		
+		// MySQL Password
+		printf ("MySQL password?: ");
+		cJSON_AddStringToObject (cj, "mysqlpass", mysqlpass);
+		
+		// MySQL Port
+		printf ("MySQL port? [3306]: ");
+		scanf ("%u", mysqlport);
+		if (*mysqlport == 0)
+			cJSON_AddNumberToObject (cj, "mysqlport", 3306);
+		else
+			cJSON_AddNumberToObject (cj, "mysqlport", *mysqlport);
+		
+		
+		// Print to file
+		FILE * nf = fopen (CONFIG_FILE, "w");
+		char * jbuff = cJSON_Print (cj);
+		if (jbuff != NULL)
+		{
+			int len = strlen(jbuff);
+			fwrite (jbuff, 1, len, nf);
+			printf ("Generated config file.\n");
+		}
+		else
+			printf ("Error: couldnt print json.\n");
+		fclose (nf);
+		printf ("Press [ENTER] to quit.");
+		gets (dp);
+	}
 	
-	// Then it allocates memory for ship blocks
+	/*
+	   Then it appears to get data from a packet (logon packet?) for setting up network
+	   parameters for the ship and for connecting to the logon server.  It then tries to connect.
 	
-	// Then, much later after other files have loaded, it loads ban data
+	   Then it allocates memory for ship blocks
 	
-	// Then it opens up the ship ports for connections
+	   Then, much later after other files have loaded, it loads ban data
 	
-	// Then what looks like tray icon loading
+	   Then it opens up the ship ports for connections
 	
-	// Then a MASSIVE for loop at the end, im guessing for actual ship stuff
+	   Then what looks like tray icon loading
+	
+	   Then a MASSIVE for loop at the end, im guessing for actual ship stuff
+	 */
 #endif
 }
